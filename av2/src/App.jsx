67b74920975ee2b8da5a422a2302f168ba38cc21
Victor Nascimento Aguiar <112593695+victorNascimentoAguiar@users.noreply.github.com>
2023-12-08 18:53:08 -0300

    projeto av2

---
+++ av2/src/App.jsx
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+// Componente reutilizável para campos de formulário
+const FormField = ({ label, type, value, onChange, required }) => (
+  <div>
+    <label>
+      {label}:
+      <input type={type} value={value} onChange={onChange} required={required} />
+    </label>
+  </div>
+);
+
+// Componente principal para o formulário de cadastro
+const CadastroForm = ({ onCadastro }) => {
+  const [nome, setNome] = useState('');
+  const [descricao, setDescricao] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Verifica se os campos obrigatórios estão preenchidos
+    if (nome.trim() === '' || descricao.trim() === '') {
+      alert('Por favor, preencha todos os campos obrigatórios.');
+      return;
+    }
+
+    // Adiciona o item à lista no localStorage
+    const novoItem = { nome, descricao };
+    const itens = JSON.parse(localStorage.getItem('itens')) || [];
+    itens.push(novoItem);
+    localStorage.setItem('itens', JSON.stringify(itens));
+
+    // Limpa os campos do formulário
+    setNome('');
+    setDescricao('');
+
+    // Executa a função de cadastro passada como prop
+    onCadastro();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Formulário de Cadastro</h2>
+      <FormField label="Nome" type="text" value={nome} onChange={(e) => setNome(e.target.value)} required />
+      <FormField label="Descrição" type="text" value={descricao} onChange={(e) => setDescricao(e.target.value)} required />
+      <button type="submit">Cadastrar</button>
+    </form>
+  );
+};
+
+// Componente para exibir a lista de itens cadastrados
+const ListaItens = () => {
+  const [itens, setItens] = useState([]);
+
+  useEffect(() => {
+    // Atualiza a lista de itens quando o componente é montado
+    const itensSalvos = JSON.parse(localStorage.getItem('itens')) || [];
+    setItens(itensSalvos);
+  }, []);
+
+  return (
+    <div>
+      <h2>Lista de Itens</h2>
+      <ul>
+        {itens.map((item, index) => (
+          <li key={index}>
+            <strong>{item.nome}</strong>: {item.descricao}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+// Componente principal
+const App = () => {
+  const handleCadastro = () => {
+    // Atualiza o estado para forçar a atualização da ListaItens
+    // e exibir o item recém-cadastrado
+    setAtualizarLista((prevState) => !prevState);
+  };
+
+  const [atualizarLista, setAtualizarLista] = useState(false);
+
+  return (
+    <div className="App">
+      <CadastroForm onCadastro={handleCadastro} />
+      <ListaItens key={atualizarLista} />
+    </div>
+  );
+};
+
+export default App;
